815a0d501219ce2d9c5192ad39e7a7981e4e3d20
Aphatheology <mustapha.abdulkareem@saritay.com>
2024-01-08 18:23:04 +0100

    getting started

---
+++ src/main/java/com/aphatheology/attendancelogger/AttendanceLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.aphatheology.attendancelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aphatheology/attendancelogger/AttendanceLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aphatheology.attendancelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
